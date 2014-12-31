4e721eb9a0e0dddbe94c8e22b2fbe649888eb87b
Kevin Payravi <kevinpayravi@gmail.com>
2014-12-31 02:15:52 -0500

    Adding header

--- grabJSON.php
+++ grabJSON.php
@@ -1,3 +1,15 @@
+<!--
+/*!
+ * Wikipedia Traffic Grapher
+ * 
+ * Version: 1.0
+ *
+ * Copyright 2014 Kevin Payravi (SuperHamster @ en.wiki)
+ * Released under the GNU General Public License
+ * https://github.com/KevinPayravi/Wikipedia-Traffic-Grapher/blob/master/LICENSE
+ */
+-->
+
 <?php
 	$urlBase = 'http://stats.grok.se/json/en/';
 	$url = $urlBase . $_GET['date'] . "/" . $_GET['article'];
